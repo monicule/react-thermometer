865fc452afdd8305d774c90c14019d7851aaa953
Monika <laiskutis.monikai@gmail.com>
2024-09-06 11:36:16 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Temperature from './components/temperature/Temperature'
+import Buttons from './components/buttons/Buttons'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <div>
+      <p>Thermometer</p>
+      <Temperature />
+     </div>
+     <Buttons />
+    </>
+  )
+}
+
+export default App
